4f2f5329f920c01833e30eaebd4701f7c36c5694
Christian Robinson <chrisdrobbo@gmail.com>
2020-11-30 21:29:36 +0000

    added #possilbe_moves to Rook class

--- lib/rook.rb
+++ lib/rook.rb
@@ -10,4 +10,34 @@ class Rook < Piece
   def unicode
     "\u265C"
   end
+
+  def valid_move?(chess, player)
+    start_coordinates = chess.start_coordinates
+    finish_coordinates = chess.finish_coordinates
+    
+    squares = possible_moves(start_coordinates)
+  
+    if squares.includes
+      
+
+  end
+
+  def possible_moves(start_coordinates) 
+
+    until i > 8
+      i = 1
+      moves[0] << [start_coordinates[1], i]
+      i += 1
+    end
+
+    until i > 8 
+      i = 1
+      moves[1] << [start_coordinates[0], i]
+      i += 1
+    end
+
+  end
+
+  
+
 end
